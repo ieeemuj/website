5f3e048e00f3d89f5fa96def8e3ac1903dfc24f1
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-07-02 19:46:46 +0530

    feat: add theme

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
+import { ChakraProvider, ThemeProvider, CSSReset } from "@chakra-ui/react";
+import Layout from "../components/Layout";
+
+// theme
+import theme from "../theme/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <CSSReset />
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
---
+++ theme/theme.js
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+  fonts: {
+    heading: "Inter",
+    body: "Inter",
+  },
+});
+
+export default theme;
