811807484dcb36ded1af580088cea234ce1e6d4e
somil24 <somil2402@gmail.com>
2021-07-08 22:10:33 +0530

    Styled Navbar

--- components/Layout/Navbar/NavLink.jsx
+++ components/Layout/Navbar/NavLink.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { Link } from '@chakra-ui/react';
+import {useRouter} from "next/router"
 
-const NavLink = ({ to, children }) => (
+
+const NavLink = ({ to, children }) => {
+
+  const router=useRouter()
+  
+  return(
+  <>
+   
   <Link
     px={2}
     py={1}
+    
     _hover={{
       textDecoration: 'none',
+      backgroundColor:'#3182CE',
+      borderRadius:'6px',
+    
+      
     }}
+    style={{borderRadius:'6px'}}
     href={to}
+    bg={router.pathname===to?"#3182CE":""}
+   
+    color={router.pathname===to?"white":""}
   >
+  
     {children}
   </Link>
-);
+ 
+  </>)
+};
 
 export default NavLink;
--- components/Layout/Navbar/index.jsx
+++ components/Layout/Navbar/index.jsx
@@ -14,19 +14,22 @@ import NavMenuItem from './NavMenuItem';
 // CONSTANTS
 import NAV_LINKS from '../../../constants/NavLinks';
 
-const NavLinks = () => (
-  <>
-    <NavLink to="/">Home</NavLink>
+const NavLinks = () => {
+  
+  return(<>
+    <NavLink to="/" >Home</NavLink>
     {NAV_LINKS.map((navMenuLink) => (
       <NavMenuItem navMenu={navMenuLink} key={navMenuLink.title} />
     ))}
-    <NavLink to="/events">Event</NavLink>
+    <NavLink to="/events" >Event</NavLink>
     <NavLink to="/projects">Projects</NavLink>
     <NavLink to="/ieeewie">IEEE WIE</NavLink>
     <NavLink to="/ieeecs">IEEE CS</NavLink>
     <NavLink to="/join-us">Join Us</NavLink>
-  </>
-);
+  </>)
+};
+
+
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -36,7 +39,7 @@ const Navbar = () => {
       <Box px={[4, 4, 16, 32]}>
         <Flex h={16} alignItems="center" justifyContent="space-between">
           <Box>IEEE SB MUJ</Box>
-          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
+          <HStack as="nav" spacing={4} display={{ base: 'none', xl: 'flex' }}>
             <NavLinks />
           </HStack>
           <IconButton
@@ -47,13 +50,13 @@ const Navbar = () => {
             size="md"
             icon={isOpen ? <MdClose /> : <MdMenu />}
             aria-label="Open Menu"
-            display={{ md: 'none' }}
+            display={{ xl: 'none' }}
             onClick={isOpen ? onClose : onOpen}
           />
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
+          <Box pb={4} display={{ xl: 'none' }}>
             <Stack as="nav" spacing={4}>
               <NavLinks />
             </Stack>
