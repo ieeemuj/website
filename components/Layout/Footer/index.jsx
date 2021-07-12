700d01c95f766157c7f0c97ef67c6bc2bf9beeda
Aditya Khattar <khattaraditya2001@users.noreply.github.com>
2021-07-13 05:24:09 +0530

    completed footer design

--- components/Layout/Footer/index.jsx
+++ components/Layout/Footer/index.jsx
@@ -10,24 +10,32 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
-import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { FaInstagram, FaTwitter, FaYoutube, FaTelegramPlane } from 'react-icons/fa';
+
+import { HiLocationMarker, HiMailOpen } from "react-icons/hi";
+
+import styles from '../../../styles/Home.module.css';
 
 const SocialButton = ({ children, label, href }) => (
   <chakra.button
-    bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-    rounded="full"
-    w={8}
-    h={8}
     cursor="pointer"
     as="a"
     href={href}
     display="inline-flex"
     alignItems="center"
     justifyContent="center"
-    transition="background 0.3s ease"
-    _hover={{
-      bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-    }}
+  >
+    <VisuallyHidden>{label}</VisuallyHidden>
+    {children}
+  </chakra.button>
+);
+
+const Level1Buttons = ({ children, label, href }) => (
+  <chakra.button
+    rounded="full"
+    display="inline-flex"
+    alignItems="center"
+    justifyContent="center"
   >
     <VisuallyHidden>{label}</VisuallyHidden>
     {children}
@@ -40,46 +48,87 @@ const Footer = () => {
     'Events',
     'Projects',
     'Team',
-    // "IEEE-WIE",
-    // "IEEE-CS",
-    // "JOIN-US",
+    'IEEE-WIE',
+    'IEEE-CS',
+    'Join Us',
   ];
 
   return (
-    <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}
-    >
-      <Container as={Stack} maxW="6xl" py={10}>
-        <SimpleGrid
-          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr' }}
-          spacing={8}
-        >
-          <Stack spacing={6}>
-            <Box>IEEE SB MUJ</Box>
-            <Text fontSize="sm">© 2021 IEEE SB MUJ. All rights reserved</Text>
-            <Stack direction="row" spacing={6}>
-              <SocialButton label="Twitter" href="#">
-                <FaTwitter />
-              </SocialButton>
-              <SocialButton label="YouTube" href="#">
-                <FaYoutube />
-              </SocialButton>
-              <SocialButton label="Instagram" href="#">
-                <FaInstagram />
-              </SocialButton>
-            </Stack>
-          </Stack>
-          <Stack align="flex-start">
-            {Links.map((link) => (
+    <div className={styles.footer}>
+      <div className={styles.level1}>
+        <a href="#" className={styles.location}>
+          <Level1Buttons label="Location" href="#">
+            <div className={styles.footerLevel1Icon}>
+              <HiLocationMarker />
+            </div>
+          </Level1Buttons>
+          <div className={styles.locationText}>
+            <span>Find Us.</span>
+            <span>Lorem ipsum, dolor sit amet</span>
+          </div>
+        </a>
+        <a href="#" className={styles.subscribe}>
+          <Level1Buttons label="Subscribe" href="#">
+            <div className={styles.footerLevel1Icon}>
+              <FaTelegramPlane />
+            </div>
+          </Level1Buttons>
+          <div className={styles.subscribeText}>
+            <span>Subscribe for the Latest Tech News.</span>
+            <span>Lorem ipsum, dolor sit amet</span>
+          </div>
+        </a>
+        <a href="#" className={styles.email}>
+          <Level1Buttons label="Email" href="#">
+            <div className={styles.footerLevel1Icon}>
+              <HiMailOpen />
+            </div>
+          </Level1Buttons>
+          <div className={styles.emailText}>
+            <span>Mail Us.</span>
+            <span>someone@example.com</span>
+          </div>
+        </a>
+      </div>
+      <hr className={styles.footerRule}></hr>
+      <div className={styles.level2}>
+        <div className={styles.logoContainer}>
+          <a href="#"><img src="https://www.dawex.com/hubfs/migration-assets/img/events/ieee-logo.svg" alt="logo" /></a>
+        </div>
+        <div className={styles.linksContainer}>
+          {Links.map((link) => (
+            <div className={styles.link}>
               <Link key={link} href={link.toLowerCase()}>
                 {link}
               </Link>
-            ))}
-          </Stack>
-        </SimpleGrid>
-      </Container>
-    </Box>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.level3}>
+        <div className={styles.copyrightInfo}>
+          © 2021 IEEE SB MUJ. All rights reserved
+        </div>
+        <div className={styles.socialMedia}>
+          <SocialButton label="Twitter" href="#">
+            <div className={styles.footerLevel3Icon}>
+              <FaTwitter />
+            </div>
+          </SocialButton>
+          <SocialButton label="YouTube" href="#">
+            <div className={styles.footerLevel3Icon}>
+              <FaYoutube />
+            </div>
+          </SocialButton>
+          <SocialButton label="Instagram" href="#">
+            <div className={styles.footerLevel3Icon}>
+              <FaInstagram />
+            </div>
+          </SocialButton>
+        </div>
+      </div>
+    </div>
+    
   );
 };
 
