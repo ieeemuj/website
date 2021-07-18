6f233f69c399708bb382f5aa90ad20a3e51057d2
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2021-07-19 01:46:36 +0530

    Add events page

---
+++ components/Events/PastEvents/PastEventsHeader.jsx
@@ -0,0 +1,15 @@
+import { Heading } from '@chakra-ui/react';
+
+const PastEventsHeader = () => (
+  <>
+    <Heading
+      width="100%"
+      size="lg"
+      textAlign="center"
+    >
+      Past Events
+    </Heading>
+  </>
+);
+
+export default PastEventsHeader;
