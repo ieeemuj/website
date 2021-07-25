8219ba60cc65d9bdd7627afc3d88a5c0ea268ba5
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2021-07-26 01:56:53 +0530

    Add svg

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -4,9 +4,8 @@ import Script from 'next/script';
 // components
 import { useEffect, useRef, useState } from 'react';
 import {
-  Box, Heading, Text, VStack,
+  Box, Heading, HStack, Text, VStack,
 } from '@chakra-ui/react';
-import GLOBE from 'vanta/dist/vanta.globe.min';
 import Section from '../../components/Home/Section';
 import SectionHeader from '../../components/Home/Section/SectionHeader';
 import AboutUsHeader from '../../components/Home/AboutUs/AboutUsHeader';
@@ -21,40 +20,16 @@ import homeContent from '../../content/pages/home.json';
 import { getAllActiveEvents } from '../../lib/events';
 import ActiveEventsCarousel from '../../components/Events/ActiveEvents/ActiveEventsCarousel';
 import ResponsiveContainer from '../../components/Layout/ResponsiveContainer';
+import HeaderSvg from '../../public/svg/header.svg';
 
 const Home = () => {
   const [activeEventsData, setActiveEventsData] = useState([]);
-  const [vantaEffect, setVantaEffect] = useState(0);
   const myRef = useRef(null);
 
   useEffect(() => {
     const activeEvents = getAllActiveEvents();
     setActiveEventsData(activeEvents);
   }, []);
-
-  useEffect(() => {
-    if (window.innerWidth >= 1280) {
-      if (!vantaEffect && window.THREE) {
-        setVantaEffect(GLOBE({
-          el: myRef.current,
-          mouseControls: true,
-          touchControls: false,
-          gyroControls: false,
-          minHeight: 200.00,
-          minWidth: 200.00,
-          scale: 1.00,
-          scaleMobile: 1.00,
-          size: 0.8,
-          backgroundColor: 0x00132B,
-          color: 0x0087ca,
-          points: 0,
-        }));
-      }
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
 
   return (
     <main>
@@ -71,42 +46,49 @@ const Home = () => {
         width="100%"
         bgColor="brand.700"
         height="calc(100vh - 96px)"
+        borderBottom="solid #0087ca"
       >
         <ResponsiveContainer>
-          <VStack
-            width="100%"
+          <HStack
             height="calc(100vh - 96px)"
-            boxSizing="border-box"
-            justify="center"
-            alignItems={['center', 'center', 'center', 'center', 'flex-start']}
-            // paddingTop="calc(0.2 * 100vh)"
-            spacing="5"
+            width="100%"
+            spacing="10"
           >
-            <Heading
-              fontSize={['5xl', '5xl', '7xl', '8xl', '7xl', '8xl']}
-              color="white"
-              textAlign="center"
+            <VStack
+              width={['100%', '100%', '100%', '100%', '45%']}
+              height="calc(100vh - 96px)"
+              boxSizing="border-box"
+              justify="center"
+              alignItems={['center', 'center', 'center', 'center', 'flex-start']}
+            // paddingTop="calc(0.2 * 100vh)"
+              spacing="5"
             >
-              Your Vision,
-            </Heading>
-            <Heading
-              fontSize={['5xl', '5xl', '7xl', '8xl', '7xl', '8xl']}
-              color="white"
-              textAlign="center"
+              <Heading
+                fontSize={['5xl', '6xl', '7xl', '7xl', '6xl', '6xl']}
+                color="white"
+                textAlign={['center', 'center', 'center', 'center', 'left']}
+              >
+                Your Growth is our Success.
+              </Heading>
+              <Text
+                width={['100%', '100%', '100%', '100%', '100%']}
+                color="white"
+                fontSize={['md', 'xl']}
+                textAlign={['center', 'center', 'center', 'center', 'left']}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Aenean bibendum sit erat molestie egestas nullam suspendisse.
+                Lorem at dapibus ut at vulputate ut lobortis a. Tempus pulvinar felis, convallis eu.
+              </Text>
+            </VStack>
+            <Box
+              display={['none', 'none', 'none', 'none', 'inline-block']}
+              width="60%"
+              height="100%"
             >
-              Our Passion
-            </Heading>
-            <Text
-              width={['100%', '100%', '100%', '100%', '50%']}
-              color="white"
-              fontSize={['md', 'xl']}
-              textAlign={['center', 'center', 'center', 'center', 'left']}
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Aenean bibendum sit erat molestie egestas nullam suspendisse.
-              Lorem at dapibus ut at vulputate ut lobortis a. Tempus pulvinar felis, convallis eu.
-            </Text>
-          </VStack>
+              <HeaderSvg height="100%" />
+            </Box>
+          </HStack>
         </ResponsiveContainer>
       </Box>
       <Section>
