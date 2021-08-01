ce5fd9248e8301857524d96540fe53b5dcccdabd
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2021-08-01 21:22:50 +0530

    Add: progress bar and font optimization

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" prefix="og: https://ogp.me/ns#">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
