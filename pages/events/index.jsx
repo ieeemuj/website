6bc76802d3477923df5518aa33727e0276c5cb00
Ta5r <srv.tanay@gmail.com>
2021-08-30 23:08:49 +0530

    made ui for search-bar and filter-btns for EVENTS page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { DateTime } from 'luxon';
-import { Heading, Text } from '@chakra-ui/react';
+import {
+  Heading,
+  Text,
+  Box,
+  Flex,
+  Stack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Menu,
+  MenuButton,
+  Button,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
+import { ChevronDownIcon, SearchIcon } from '@chakra-ui/icons';
 import { NextSeo } from 'next-seo';
 import Section from '../../components/Home/Section';
 import SectionHeader from '../../components/Home/Section/SectionHeader';
@@ -14,6 +29,7 @@ import { getAllEvents } from '../../cms/queries/event';
 import getStatus from '../../utils';
 import TitleHeader from '../../components/Layout/TitleHeader';
 import FadeInUp from '../../components/FadeInUp';
+import ResponsiveContainer from '../../components/Layout/ResponsiveContainer';
 
 const Events = ({ allEvents }) => {
   const [activeEventsData, setActiveEventsData] = useState([]);
@@ -104,6 +120,49 @@ const Events = ({ allEvents }) => {
         <SectionHeader>
           <PastEventsHeader />
         </SectionHeader>
+        {/* </Section> */}
+        <Box width="100%" bgColor="white" paddingTop="16px" paddingBottom="16px">
+          <ResponsiveContainer>
+            <Flex minH="70px" marginY="32px">
+              <Stack spacing="10" alignItems="center" flexGrow="1" direction={['column', 'row']}>
+                <InputGroup backgroundColor="gray.50">
+                  <InputLeftElement
+                    pointerEvents="none"
+                  >
+                    <SearchIcon marginTop="5px" marginLeft="5px" boxSize="20px" color="gray.300" />
+                  </InputLeftElement>
+                  <Input type="text" placeholder="Search Events" size="lg" />
+                </InputGroup>
+
+                <Menu>
+                  <MenuButton width="150px" as={Button} size="lg" backgroundColor="#003366" color="#fff" _hover={{ bg: '#003366' }} _expanded={{ bg: '#003366' }} _focus={{ boxShadow: 'outline' }}>
+                    Year
+                    <ChevronDownIcon />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>2021</MenuItem>
+                    <MenuItem>2020</MenuItem>
+                    <MenuItem>2019</MenuItem>
+                    <MenuItem>2018</MenuItem>
+                  </MenuList>
+
+                </Menu>
+                <Menu>
+                  <MenuButton width="150px" as={Button} size="lg" backgroundColor="#003366" color="#fff" paddingRight="35px" _hover={{ bg: '#003366' }} _expanded={{ bg: '#003366' }} _focus={{ boxShadow: 'outline' }}>
+                    Society
+                    <ChevronDownIcon />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>IEEE SB MUJ</MenuItem>
+                    <MenuItem>IEEE CS MUJ</MenuItem>
+                    <MenuItem>IEEE WIE MUJ</MenuItem>
+                  </MenuList>
+                </Menu>
+              </Stack>
+            </Flex>
+          </ResponsiveContainer>
+        </Box>
+        {/* <Section> */}
         <SectionContent>
           <PastEventsGrid
             allEventsData={pastEventsData}
